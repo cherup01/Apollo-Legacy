373c899f0b11c6f392ce78e9fbce36adc6c08db9
_Icey._ <wesleyvanneck1012@gmail.com>
2018-01-12 16:45:33 +0100

    Update AsyncWorker.php

--- src/pocketmine/scheduler/AsyncWorker.php
+++ src/pocketmine/scheduler/AsyncWorker.php
@@ -45,7 +45,7 @@ class AsyncWorker extends Worker{
 		set_error_handler('\pocketmine\error_handler');
 
 		$this->registerClassLoader();
-		$this->logger->registerStatic();
+		//$this->logger->registerStatic();
 		gc_enable();
 
 		if($this->memoryLimit > 0){
